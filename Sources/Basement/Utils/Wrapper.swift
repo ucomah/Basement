2bd1ffde33f506375831425ecc4117c5da28034a
Eugene <>
2021-09-18 21:05:14 +0300

    Realm wrapper.

---
+++ Sources/Basement/Utils/Wrapper.swift
@@ -0,0 +1,29 @@
+import RealmSwift
+import Realm
+
+/// This wrapper is needed to be able to store Realm instance as ThreadSpecificVariable.
+final class RealmWrapper {
+    
+    let realm: Realm
+
+    init(conf: Realm.Configuration) throws {
+        realm = try Realm(configuration: conf)
+    }
+
+    init(realm: Realm) {
+        self.realm = realm
+    }
+}
+
+extension Object {
+
+    public static func from<T: Object>(value: Any) -> T {
+        let obj = T()
+        RLMInitializeWithValue(obj, value, .partialPrivateShared())
+        return obj
+    }
+
+    public static func from<T: Object>(dictionary: [String: Any?]) -> T {
+        return from(value: dictionary)
+    }
+}
