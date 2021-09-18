2d6e51343d9414dc410e752459795b95db3d48d2
Eugene <>
2021-09-18 21:04:35 +0300

    FolderPath added

---
+++ Sources/Basement/Utils/FolderPath.swift
@@ -0,0 +1,30 @@
+public protocol LocalPathRepresentable {
+    var searchPath: FileManager.SearchPathDirectory { get }
+    var subPath: String? { get }
+}
+
+public extension LocalPathRepresentable {
+    func url() throws -> URL {
+        let list = NSSearchPathForDirectoriesInDomains(searchPath, .userDomainMask, true)
+        guard let dir = list.first else {
+            throw CocoaError.error(CocoaError.Code(rawValue: NSFileReadUnknownError))
+        }
+        if let p = subPath {
+            return URL(fileURLWithPath: dir).appendingPathComponent(p)
+        }
+        return URL(fileURLWithPath: dir)
+    }
+}
+
+@frozen
+public struct FolderPath: LocalPathRepresentable {
+    public let searchPath: FileManager.SearchPathDirectory
+    public let subPath: String?
+
+    public init(_ directory: FileManager.SearchPathDirectory, subPath: String? = nil) {
+        self.searchPath = directory
+        self.subPath = subPath
+    }
+
+    public static var `default`: FolderPath { .init(.documentDirectory, subPath: "Realm") }
+}
