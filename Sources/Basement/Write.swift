ec9e6b11bc99df56a354de0609e249937228d6f0
Eugene <>
2021-09-29 13:11:20 +0300

    Async write.

--- Sources/Basement/Write.swift
+++ Sources/Basement/Write.swift
@@ -24,4 +24,41 @@ public final class WriteTransaction {
     public func delete<T: Sequence>(in sequence: T) where T.Iterator.Element: Object {
         realm.delete(sequence)
     }
+    
+    public func async<T: ThreadConfined>(_ passedObject: T,
+                                         queue: DispatchQueue? = nil,
+                                         errorHandler: @escaping ((_ error: Swift.Error) -> Void) = { _ in return },
+                                         closure: @escaping ((T, WriteTransaction) throws -> Void)) {
+        let objectReference: ThreadSafeReference<T>? = passedObject.realm != nil ? ThreadSafeReference(to: passedObject) : nil
+        let configuration = self.realm.configuration
+        let queue = queue ?? backgroundQueue
+        queue.async { [weak self] in
+            autoreleasepool {
+                do {
+                    guard let this = self else {
+                        throw Realm.Error(.fail)
+                    }
+                    let realm = try Realm(configuration: configuration)
+                    let object: T
+                    if let ref = objectReference {
+                        if let obj = realm.resolve(ref) {
+                            // Resolve within the transaction to ensure you get the latest changes from other threads
+                            object = obj
+                        } else {
+                            throw Realm.Error(.fail)
+                        }
+                    } else {
+                        object = passedObject
+                    }
+                    try closure(object, this)
+                } catch {
+                    errorHandler(error)
+                }
+            }
+        }
+    }
+}
+
+var backgroundQueue: DispatchQueue {
+    .global(qos: .background)
 }
