24eac626cf26be9697c79755cc5c5ad2defdb14c
Eugene <>
2021-10-14 11:16:00 +0300

    Nothrow for Combine extension.

--- Sources/Basement/Actions+Combine.swift
+++ Sources/Basement/Actions+Combine.swift
@@ -52,14 +52,14 @@ extension Container {
     
     public func publishWrite<T: ThreadConfined>(_ obj: T,
                                               on queue: DispatchQueue?,
-                                              block: @escaping ((T, WriteTransaction) throws -> Void)) -> AnyPublisher<T, Error> {
+                                              block: @escaping ((T, WriteTransaction) -> Void)) -> AnyPublisher<T, Error> {
         Future<T, Error> { [weak self] seal in
             guard let this = self else {
                 seal(.failure(Realm.Error.callFailed))
                 return
             }
             this.writeAsync(obj, queue: queue) { obj, tr in
-                try block(obj, tr)
+                block(obj, tr)
                 seal(.success(obj))
             } errorHandler: { error in
                 seal(.failure(error))
