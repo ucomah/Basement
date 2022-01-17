758fbd4d30dd434121b9d1f995ed21a91522a031
Eugene <>
2022-01-17 14:30:23 +0200

    Package updated.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,14 +8,14 @@ let targetName = packageName
 
 let package = Package(
     name: packageName,
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v11)],
     products: [
         .library(
             name: packageName,
             targets: [targetName]),
     ],
     dependencies: [
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.0.0")
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.15.0")
     ],
     targets: [
         .target(
